3cf3cf428f370dcd647034723400cf9976ee4f50
VladiOrt <vladi_ortega@hotmail.com>
2024-11-07 03:11:08 -0600

    feat : se agrega configuracion para conexion a mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/toDo'),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
