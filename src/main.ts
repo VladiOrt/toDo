2f458c80c14b083427b98a22771f627003b59ceb
VladiOrt <vladi_ortega@hotmail.com>
2024-11-07 03:22:48 -0600

    feat : Se agrega respuesta general del proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseFormatInterceptor } from './response-format.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ResponseFormatInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
