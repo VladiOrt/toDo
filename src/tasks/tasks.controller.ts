43160d26a9ba4f36af6eb7d4907d254baaa3f2b0
VladiOrt <vladi_ortega@hotmail.com>
2024-11-10 04:13:38 -0600

    Se crea el endpoint de creacion

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
-@Controller('tasks')
-export class TasksController {}
+@Controller('api/tasks')
+export class TasksController {
+    constructor(private readonly taskService:TasksService){}
+
+    @Post('/')
+    async createTask(@Body() createTaskDto: CreateTaskDto){
+        try{        
+            const resultTask = await this.taskService.create(createTaskDto);
+
+            return {
+                ok: true,
+                msg: 'Tarea creada con exíto',
+                data: 'data'
+            }
+        }catch(error){
+            return { 
+                ok: false,
+                msg: 'Ocurrio un error al Crear una tarea',
+                data: error
+            }
+        };
+    }
+
+}
