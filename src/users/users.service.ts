84db0b2cf8b88dcb8e334ae67fda2d394064c8de
VladiOrt <vladi_ortega@hotmail.com>
2024-11-11 04:40:15 -0600

    feat: Se actualiza service para uso de dto

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,14 +3,16 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './users.schema';
 import * as bcrypt from 'bcryptjs';
+import { CreateUserDto } from './user-register.dto';
 
 @Injectable()
 export class UsersService {
     constructor(@InjectModel(User.name) private userModel: Model<UserDocument>){}
 
-    async createUser(username: string, password: string): Promise<User>{
+    async createUser(createUserDto: CreateUserDto): Promise<User>{
+        const { email, password } = createUserDto
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new this.userModel({ username, password: hashedPassword });
+        const newUser = new this.userModel({ email, password: hashedPassword });
         return newUser.save();
     }
 
