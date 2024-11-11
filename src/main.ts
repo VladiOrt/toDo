df15150fe7fa7143adcce97cf51befc5f34db9ad
VladiOrt <vladi_ortega@hotmail.com>
2024-11-11 04:41:05 -0600

    feat: Se configura para validacion de variables

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseFormatInterceptor } from './response-format.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseFormatInterceptor());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
