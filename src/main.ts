2c088775ce6f7dd3ef6514194c41ff8d4f7dd75a
VladiOrt <vladi_ortega@hotmail.com>
2024-11-12 04:09:15 -0600

    docs: Se agrega swagger para documentar el proyecto

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseFormatInterceptor } from './response-format.interceptor';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('Documentación de la API de Tareas')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   app.useGlobalInterceptors(new ResponseFormatInterceptor());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(process.env.PORT ?? 3000);
