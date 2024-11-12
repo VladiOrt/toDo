f2bd952c193a77d57540bdb51fda9e5c15707ba3
VladiOrt <vladi_ortega@hotmail.com>
2024-11-12 04:08:35 -0600

    docs: Se agrega documentacion para endpoint del registro

--- src/users/user-register.dto.ts
+++ src/users/user-register.dto.ts
@@ -1,12 +1,15 @@
 import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
-  
+
+  @ApiProperty({ example: 'usuario@example.com', description: 'El correo del usuario' })
   @IsString()
   @IsEmail({}, { message: 'El email no es valido'})
   @IsNotEmpty({ message: 'El email es obligatorio'})
   email: string;
   
+  @ApiProperty({ example: 'contraseña123', description: 'La contraseña del usuario' })
   @IsString({ message: 'La contraseña debe ser un string' } )
   @IsNotEmpty({ message: 'La contraseña es obligatoria' } )
   password: string;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,33 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './user-register.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+const exampleResponse ={  
+    "status": "success",
+    "message": "Request successful",
+    "data": {
+        "ok": true,
+        "msg": "Usuario creado con exíto",
+        "data": {
+            "email": "testuser@gmail.com",
+            "password": "$2a$10$EvV2GWDC.eEbpZyAjQJHneXdHoVPSr5c/ZlZ9ChBVxCvSGT73EJay",
+            "_id": "6733229a31866f83345e2b62",
+            "__v": 0
+        }
+    }
+}
+
+
+@ApiTags('Registrar Usuario')
 @Controller('api/register')
 export class UsersController {
     constructor(private readonly userService:  UsersService){}
 
     @Post('/')
+    @ApiOperation({summary: 'Registrar Usuario'})
+    @ApiResponse({ status: 200, description: JSON.stringify(exampleResponse) })
+    @ApiResponse({ status: 401, description: 'Ocurrio un error al crear el usuario' })
     async register(@Body()  createUserDto: CreateUserDto){
         try{        
             const resultRegister= await this.userService.createUser(createUserDto);
