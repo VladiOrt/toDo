a4a2cf950410b6c1c82accc81f950a3d83a12a5b
VladiOrt <vladi_ortega@hotmail.com>
2024-11-12 04:08:46 -0600

    docs: Se agrega documentacion para endpoint del login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/user-register.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { exampleResponseAuth } from 'src/utils/response-example';
 
+@ApiTags('Login')
 @Controller('api/login')
 export class AuthController {
     constructor(private readonly authService: AuthService){}
 
     @Post('/')
+    @ApiOperation({summary: 'Iniciar sesión'})
+    @ApiResponse({status:200, description: JSON.stringify(exampleResponseAuth) })
+    @ApiResponse({ status: 401, description: 'Credenciales incorrectas.' })
     async login(@Body() createUserDto: CreateUserDto){
         const user = await this.authService.validateUser(createUserDto);
         if(!user) {
